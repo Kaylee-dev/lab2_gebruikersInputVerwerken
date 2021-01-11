a278305e38f8d38dd2089e39fd8c37f86e17cc53
Kaylee <99057337@mydavinci.nl>
2021-01-11 17:37:47 +0100

    comment toegevoegd

--- input.js
+++ input.js
@@ -1,20 +1,27 @@
-var nameQuestion = prompt("Hoeveel namen wilt u in de array stoppen?");
+var nameQuestion = prompt("Hoeveel namen wilt u in de array stoppen? (minimaal 3)");
 var names = [];
+//Een lega array om later namen in te zetten.
 
 document.write("<h2>De ingevoerde namen in de array zijn:</h2>");
 
 if (nameQuestion >= 3) {
 	for(var i = 0; i < nameQuestion; i++){
 		var question = prompt("Welke naam wilt u invoeren?");
-		names.push(question);
-
+		names.push(question); 
+		// push zet wat ingevoerd is bij question in de lege array.
 	}
+
 	for(var i = 0; i < nameQuestion; i++){
-			document.write(names[i] + "<br/>");
+		document.write(names[i] + "<br/>");
 		}
-		for(var i = nameQuestion; i >= 0; i--){
+
+		for(var i = names.length -1; i >= 0; i--){
 			document.write(names[i] + "<br/>");
-		}
+			}
+			// Hier is de for loop reversed zodat de namen ook andersom komen te staan
+
+}else{
+	alert("Vul minimaal 3 in");
 }
 
 console.log(names);
