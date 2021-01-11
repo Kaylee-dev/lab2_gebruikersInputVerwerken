57a4d664036bbf42f18d62f2f68fd4dff728be67
Kaylee <99057337@mydavinci.nl>
2021-01-11 13:18:53 +0100

    array en loops toegevoegd

--- input.js
+++ input.js
@@ -1,8 +1,20 @@
 var nameQuestion = prompt("Hoeveel namen wilt u in de array stoppen?");
+var names = [];
 
-if (nameQuestion == 3) {
-	for(i = 0; i <= 2; i++){
+document.write("<h2>De ingevoerde namen in de array zijn:</h2>");
+
+if (nameQuestion >= 3) {
+	for(var i = 0; i < nameQuestion; i++){
 		var question = prompt("Welke naam wilt u invoeren?");
-		document.write(question + "<br>");	
+		names.push(question);
+
 	}
+	for(var i = 0; i < nameQuestion; i++){
+			document.write(names[i] + "<br/>");
+		}
+		for(var i = nameQuestion; i >= 0; i--){
+			document.write(names[i] + "<br/>");
+		}
 }
+
+console.log(names);
